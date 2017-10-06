5c179a2838ce9f004159d407daa9c7d0c4d1398f
Nikolai Pawlowski <nikolaip@gmail.com>
2017-10-06 10:26:59 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -128,7 +128,7 @@ def adding_to_dennis
         }
      }
 
-		 programmer_hash[:dennis_ritchie][known_for] << "Assembly"
+		 programmer_hash[:dennis_ritchie][:known_for] << "Assembly"
 		 programmer_hash
 
 end
