e92e0f793acb17ff5ea6b2d8b75ea4cf6b807824
Nikolai Pawlowski <nikolaip@gmail.com>
2017-10-06 10:04:15 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -57,7 +57,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-	programmer_hash[:dennis_ritchie][:language]
+	programmer_hash[:dennis_ritchie][:language].to_s
 
 end
 
